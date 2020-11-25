3621efff506ad96bd5e5d93a4f1e8d29e12b27ec
Wagner Carvalho <wagncarv@gmail.com>
2020-11-25 14:23:32 -0300

    atualizacao implementacao testes

--- test/agente_test.exs
+++ test/agente_test.exs
@@ -2,7 +2,20 @@ defmodule AgenteTest do
   use ExUnit.Case
   doctest Agente
 
-  test "greets the world" do
-    assert Agente.hello() == :world
+  describe "Test return value" do
+    value = "aaa333"
+    Agente.update(value)
+    assert Agente.value == %{cursor: value}
+  end
+
+  describe "Test value" do
+    value = "aaa333"
+    Agente.update(value)
+    assert Agente.value == %{cursor: value}
+  end
+
+  describe "Test update" do
+    returned_value = Agente.update("cccxxx")
+    assert returned_value == :ok
   end
 end
